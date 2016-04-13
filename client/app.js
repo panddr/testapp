7428d6c93407ed6dcc59127bc35f7498a6aed807
panddr <panddr@gmail.com>
2016-04-13 18:43:28 +0300

    initial

---
+++ client/app.js
@@ -0,0 +1,28 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+import { Router, Route } from 'react-router';
+
+import { getOrSetUserId } from './UserId';
+import { setupRealtime } from './Realtime';
+
+import routes from '../universal/routes';
+import store from '../universal/store';
+import * as actions from '../universal/actions/PulseActions';
+
+import Root from '../universal/containers/root';
+
+import '../style/pure.css';
+import '../style/main.styl';
+import '../style/spinner.styl';
+
+
+ReactDOM.render(
+  <Root store={store} routing={routes} />,
+  document.getElementById('app')
+);
+
+// Now that we have rendered...
+setupRealtime(store, actions);
+
+// lets mutate state and set UserID as key from local storage
+store.dispatch(actions.setUserId(getOrSetUserId()));
---
+++ universal/containers/root/root.dev.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import { Router, browserHistory } from 'react-router';
+
+import DevTools from '../devTools';
+
+export default class Root extends Component {
+  render() {
+    const { store, routing } = this.props;
+    return (
+      <Provider store={store}>
+        <div>
+          <Router history={browserHistory}>
+            {routing}
+          </Router>
+
+        </div>
+      </Provider>
+    );
+  }
+};
+
